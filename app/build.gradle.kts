3447b7050d63949320724e53ea0cf09ac46517d4
Da3thVade3 <manan.gami56@gmail.com>
2026-07-16 03:36:20 +0200

    Release: v0.0.11-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.prithvi.ecovitality"
         minSdk = 34
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.0.10-alpha"
+        versionCode = 11
+        versionName = "0.0.11-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
